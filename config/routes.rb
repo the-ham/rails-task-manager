222d2196cb33142b19997edb27a434a5f3f9470f
Ambery Harris <amberyharris.ah@gmail.com>
2022-11-14 12:25:17 +1100

    02 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'tasks#all'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
